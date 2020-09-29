5c13a34d878854e6ca9f6658496974388520130f
G Subramonia Iyer <68558808+Appuaravind98@users.noreply.github.com>
2020-09-29 10:34:49 +0530

    Add files via upload

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db ={
+    screams=[
+        {
+            userHandle: "userr",
+            body: "hey guys",
+            createdAt: "2019-13-15T11:46:01.018Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ functions/util/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apiKey: "AIzaSyBm_KPvsfO7XCIr4eaOMueMTZ5qcsKYMDg",
+    authDomain: "famyou.firebaseapp.com",
+    databaseURL: "https://famyou.firebaseio.com",
+    projectId: "famyou",
+    storageBucket: "famyou.appspot.com",
+    messagingSenderId: "51898649598",
+    appId: "1:51898649598:web:965bd2bf6a86a03e0eceaf"
+  };
